632d863bd7d6a2eefbeb568f2a051b62e6b956b6
Melly <mebostjancic@edu.aau.at>
2024-03-11 09:28:00 +0100

    03_Added TCP Connection displayed the answer of the Server in an TextView

--- app/src/main/java/com/example/seii_einzelbeispiel/MainActivity.java
+++ app/src/main/java/com/example/seii_einzelbeispiel/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.seii_einzelbeispiel;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +13,80 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
+public class MainActivity extends AppCompatActivity {
+    private Button btn;
+    private Button btn_calc;
+    private TextView txtViewAnswer;
+    private EditText editText;
+
+    private String server_domain = "se2-submission.aau.at";
+    private int port = 20080;
+
+    private Socket socket;
+    private PrintWriter out;
+    private BufferedReader in;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        btn = findViewById(R.id.btn_send_mainActivity);
+        btn_calc = findViewById(R.id.btn_calculate);
+        txtViewAnswer = findViewById(R.id.txt_answer_mainActivity);
+        editText = findViewById(R.id.editTextNumber);
+
+        //https://stackoverflow.com/questions/10231309/android-button-onclick
+        btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String input = editText.getText().toString();
+                String regex = "^[0-9]+";
+
+                if (input.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Please enter a numerical input", Toast.LENGTH_SHORT).show();
+                } else if (input.matches(regex)) {
+                    Toast.makeText(getApplicationContext(), "Button clicked", Toast.LENGTH_LONG).show();
+                    //https://www.geeksforgeeks.org/what-are-threads-in-android-with-example/
+                    Thread thread = new Thread(new Runnable() {
+                        public void run() {
+                            try {
+                                socket = new Socket(server_domain, port);
+                                out = new PrintWriter(socket.getOutputStream(), true);
+                                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                                //OutPutStream
+                                out.println(input);
+
+
+                                String response = in.readLine();
+
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        txtViewAnswer.setText(response);
+                                        Toast.makeText(getApplicationContext(), "Answer received", Toast.LENGTH_LONG).show();
+
+                                    }
+                                });
+
+                                socket.close();
+
+                            } catch (UnknownHostException e) {
+                                e.printStackTrace();
+                            } catch (IOException ex) {
+                                ex.printStackTrace();
+                            }
+                        }
+                    });
+                    thread.start();
+                }
+            }
         });
     }
 }
